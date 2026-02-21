ce89260d3caafd7c1781cf57ec48a5d4c8ec84eb
Silvio Netto <silvio.netto@gmail.com>
2026-02-21 09:30:34 +0100

    Using Scheduled

--- src/main/java/com/silvionetto/ai/AiApplication.java
+++ src/main/java/com/silvionetto/ai/AiApplication.java
@@ -2,12 +2,18 @@ package com.silvionetto.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class AiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 5000)
+	public void readEmail() {
+		System.out.println("Reading email...");
 	}
 
 }
